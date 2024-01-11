970a8f3ca6a969c418ac7c1230f71c12f85b3a50
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-01-11 12:15:46 +0000

    Add basic search logic to test on remote server

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,18 @@
 class Article < ApplicationRecord
 
-    def search
-        
+    def self.search(search)
+        # search from within redis
+        # if not found, search from within db
+        #then cache to redis
+        print "searching for #{search}"
+        if !search.blank?
+
+            #save the search term to redis by an IP address
+
+            where("title LIKE ?", "%#{search}%")
+        else
+            all
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :create]
- 
+  get 'search', to: 'articles#search'
+  get 'analytics', to: 'articles#analytics'
+
+
   get 'health', to: ->(env) {
   response = 'I am healthy'
   [200, {}, [response]] 
