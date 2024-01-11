ddcd9aea05bd3144da1f7095dc0eefae8262b223
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-01-11 13:45:33 +0000

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "search-analytics.firstlovegallery.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
